564c762d43aa1030e7625d91c01b59e99cffce9a
Jenni Bradstreet <jennic15@gmail.com>
2016-07-26 19:38:04 -0700

    Add slow scroll to different parts of a page.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     });
   };
 
+  function anchorScroll(aid) {
+    var aTag = $("a[name='" + aid +"']");
+    $('html, body').animate({
+      scrollTop: aTag.offset().top - 100
+    }, 1000);
+  }
+
+  $('li > a').click(function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href').replace('#', '')
+    anchorScroll(href);
+  })
+
   $('#cornerButton').click(function() {
     $('.brand-logo').scrollView();
   });
